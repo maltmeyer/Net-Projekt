a02f438d0edb86150bcf1e94b8fbea7d91ade9f4
Dejongo <mentor_1@gmx.de>
2015-08-12 17:03:22 +0200

    Datenbank.

--- Pizzaversand/addIngredient.ascx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="addIngredient.ascx.vb" Inherits="Pizzaversand.addIngredient" %>
-
-
--- Pizzaversand/addIngredientControl.ascx.vb
+++ Pizzaversand/addIngredientControl.ascx.vb
@@ -1,7 +1,22 @@
 ﻿Public Class addIngredient
     Inherits System.Web.UI.UserControl
 
+    Dim name As String
+    Dim desc As String
+    Dim price As String
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub onConfirmClicked() Handles confirmButton.Click
+        name = ingedientNameBox.Text
+        desc = descBox.Text
+        price = priceBox.Text
+        updateTable()
+    End Sub
+
+    Protected Sub updateTable()
 
     End Sub
 

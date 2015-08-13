f62f5596ac8dde5e8709a7a03b963ede3906af0d
Markus Altmeyer <altmeyer.m@gmx.de>
2015-08-13 12:46:55 +0200

    Tetst Datenbankzugriff hinzugefügt

---
+++ Pizzaversand/TestDatenbankZutaten.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="TestDatenbankZutaten.aspx.vb" Inherits="Pizzaversand.TestDatenbankZutaten" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtPreis" runat="server"></asp:TextBox>
+        <asp:Button ID="btnAddZutat" runat="server" Text="Zutat hinzufügen" />
+        <asp:Button ID="btnShowZutaten" runat="server" Text="Zutat anzeigen" />
+        <br />
+        <asp:PlaceHolder ID="phZutaten" runat="server"></asp:PlaceHolder>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Pizzaversand/TestDatenbankZutaten.aspx.vb
@@ -0,0 +1,44 @@
+﻿Public Class TestDatenbankZutaten
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnAddZutat_Click(sender As Object, e As EventArgs) Handles btnAddZutat.Click
+        Dim name As String = txtName.Text
+        Dim preisStr As String = txtPreis.Text
+        preisStr = preisStr.Replace(",", ".")
+        Dim preis As Double = Val(preisStr)
+
+        Dim dataset As New DataSetZutatenTableAdapters.ZutatenTableAdapter
+
+        dataset.InsertZutat(name, preis)
+
+
+
+    End Sub
+
+    Private Sub btnShowZutaten_Click(sender As Object, e As EventArgs) Handles btnShowZutaten.Click
+        Dim dataset As New DataSetZutatenTableAdapters.ZutatenTableAdapter
+
+
+        Dim dataTable As Data.DataTable = dataset.GetData()
+
+        If dataTable.Rows.Count > 0 Then
+            '    Me.phContent.Controls.Add(New LiteralControl( _
+            'CType(dtContent.Rows(0), dsPageContent.PageRow).Content))
+            phZutaten.Controls.Clear()
+
+            For Each row As DataSetZutaten.ZutatenRow In dataTable.Rows
+                'row.name
+                Dim preisStr As String = String.Format("{0:0.00}", row.Preis)
+                phZutaten.Controls.Add(New LiteralControl(row.Name & " - " & preisStr & "€" & "<br />"))
+            Next
+        End If
+
+
+    End Sub
+
+
+End Class

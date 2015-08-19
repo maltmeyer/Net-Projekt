3c6a793345bd8c64f566f5a045d2d7dee5b31bac
Dejongo <mentor_1@gmx.de>
2015-08-19 12:54:43 +0200

    User-Control zur Zutatenaktualisierung erstellt.

---
+++ Pizzaversand/UpdateIngredientControl.ascx
@@ -0,0 +1,51 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="UpdateIngredientControl.ascx.vb" Inherits="Pizzaversand.WebUserControl2" %>
+<asp:PlaceHolder ID="ingredients" runat="server"></asp:PlaceHolder>
+
+<asp:Table ID="Table1" runat="server">
+    <asp:TableHeaderRow>
+        <asp:TableHeaderCell>
+            <asp:Label ID="Label2" runat="server" Text="Zutatenwahl über ID"></asp:Label>
+
+        </asp:TableHeaderCell>
+    </asp:TableHeaderRow>
+    <asp:TableRow>
+        <asp:TableCell>
+            <asp:Label ID="Label1" runat="server" Text="ZutatenId:"></asp:Label>
+        </asp:TableCell>
+        <asp:TableCell>
+            <asp:TextBox ID="idBox" runat="server" TextMode="Number"></asp:TextBox>
+        </asp:TableCell>
+        <asp:TableCell>
+            <asp:Button ID="getButton" runat="server" Text="Laden" />
+        </asp:TableCell>
+    </asp:TableRow>
+</asp:Table>
+<br />
+<asp:Table ID="Table2" runat="server" Visible="False">
+    <asp:TableHeaderRow>
+        <asp:TableHeaderCell>
+            <asp:Label ID="Label3" runat="server" Text="Gewählte Zutat bearbeiten"></asp:Label>
+        </asp:TableHeaderCell>
+    </asp:TableHeaderRow>
+    <asp:TableRow>
+        <asp:TableCell>
+            <asp:Label ID="Label4" runat="server" Text="Name der Zutat:"></asp:Label>
+        </asp:TableCell>
+        <asp:TableCell>
+            <asp:TextBox ID="nameBox" runat="server"></asp:TextBox>
+        </asp:TableCell>
+    </asp:TableRow>
+    <asp:TableRow>
+        <asp:TableCell>
+            <asp:Label ID="Label5" runat="server" Text="Preis der Zutat:"></asp:Label>
+        </asp:TableCell>
+        <asp:TableCell>
+            <asp:TextBox ID="priceBox" runat="server"></asp:TextBox>
+        </asp:TableCell>
+    </asp:TableRow>
+    <asp:TableRow>
+        <asp:TableCell>
+            <asp:Button ID="updateButton" runat="server" Text="Update" />
+        </asp:TableCell>
+    </asp:TableRow>
+</asp:Table>
---
+++ Pizzaversand/UpdateIngredientControl.ascx.vb
@@ -0,0 +1,55 @@
+﻿Imports Pizzaversand.DataSetZutatenTableAdapters
+
+Public Class WebUserControl2
+    Inherits System.Web.UI.UserControl
+
+    Dim idOfIngredient As Integer
+    Dim name As String
+    Dim price As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim dataset As New DataSetZutatenTableAdapters.ZutatenTableAdapter
+        Dim datatable As Data.DataTable = DataSet.GetData()
+
+        If datatable.Rows.Count > 0 Then
+            ingredients.Controls.Clear()
+            For Each row As DataSetZutaten.ZutatenRow In datatable.Rows
+                Dim pricestr As String = String.Format("{0:0.00}", row.Preis)
+                Dim idstr As String = String.Format("{0:00}", row.Id)
+                ingredients.Controls.Add(New LiteralControl(idstr & " - " & row.Name & " - " & pricestr & "€" & "<br />"))
+            Next
+        End If
+
+    End Sub
+
+    Private Sub getButton_Click(sender As Object, e As EventArgs) Handles getButton.Click
+        Dim id As Integer = Val(idBox.Text)
+        Dim dataset As New DataSetZutatenTableAdapters.ZutatenTableAdapter
+        Dim datatable As Data.DataTable = DataSet.GetIngredient(id)
+
+        If datatable.Rows.Count > 0 Then
+            For Each row As DataSetZutaten.ZutatenRow In datatable.Rows
+                idOfIngredient = Val(row.Id)
+                name = row.Name
+                price = String.Format("{0:0.00}", row.Preis)
+            Next
+            Table2.Visible = True
+            Label3.Text = "Gewählte Zutat bearbeiten"
+            nameBox.Text = name
+            priceBox.Text = price
+        Else
+            Table2.Visible = True
+            Label3.Text = "Id nicht vorhanden, bitte andere Zutat auswählen"
+        End If
+
+    End Sub
+
+    Private Sub updateButton_Click(sender As Object, e As EventArgs) Handles updateButton.Click
+        Dim dataset As New DataSetZutatenTableAdapters.ZutatenTableAdapter
+        Dim pricestr As String = priceBox.Text
+        pricestr = pricestr.Replace(",", ".")
+        Dim price As Double = Val(pricestr)
+        dataset.IngredientUpdate(nameBox.Text, price, idOfIngredient)
+    End Sub
+
+End Class
